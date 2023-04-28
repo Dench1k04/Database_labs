4aedb6dd0c1b6d1c7aa9fc94d01e8676ad351805
Denis <118078020+Dench1k04@users.noreply.github.com>
2023-04-28 09:19:01 +0300

    Add files via upload

--- lab3.sql
+++ lab3.sql
@@ -1,11 +1,23 @@
-SELECT * FROM cities WHERE name LIKE '%ськ';
+SELECT name 
+FROM cities 
+WHERE name LIKE '%ськ'
 
-SELECT * FROM cities WHERE name LIKE '%донец%';
+SELECT name 
+FROM cities 
+WHERE name LIKE '%донец%'
 
-SELECT CONCAT(name, ' (', region, ')') AS city FROM cities WHERE population > 100000 ORDER BY name ASC;
+SELECT CONCAT(name, ' (', region, ')') AS city_region 
+FROM cities 
+WHERE population > 100000 
+ORDER BY name ASC
 
 SELECT name,
-population,
-CONCAT(ROUND(population / 400000 * 100, 2), '%') AS percentage_of_population FROM cities ORDER BY population DESC LIMIT 50;
+population, CONCAT(ROUND(population/4000000*100, 2), '%') AS percent_total_population 
+FROM cities 
+ORDER BY population DESC 
+LIMIT 50
 
-SELECT CONCAT(name, ' - ', ROUND(population / 400000 * 100, 2), '%') AS city_percentage FROM cities WHERE population >= 40000 ORDER BY population DESC;
+SELECT CONCAT(name, ' - ', ROUND(population/4000000*100, 2), '%') AS city_population_percent 
+FROM cities 
+WHERE population >= 40000 
+ORDER BY population DESC
